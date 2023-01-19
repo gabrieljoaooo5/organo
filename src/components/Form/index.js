6835e20e5270b85735f134a20509d1efec43a614
gabrieljaooo5 <joaogabriel1598@gmail.com>
2023-01-19 09:27:03 -0300

    Create team component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,16 +6,6 @@ import './Form.css'
 
 const Form = (props) => {
 
-    const teams = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
-
     const [name, setName] = useState('')
     const [role, setRole] = useState('')
     const [image, setImage] = useState('')
@@ -33,7 +23,7 @@ const Form = (props) => {
                 <TextField required={true} label="Nome" placeholder="Digite seu nome" value={name} onChange={value => setName(value)} />
                 <TextField required={true} label="Cargo" placeholder="Digite seu cargo" value={role} onChange={value => setRole(value)} />
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem" value={image} onChange={value => setImage(value)} />
-                <DropdownList required={true} label="Time" items={teams} value={team} onChange={value => setTeam(value)} />
+                <DropdownList required={true} label="Time" items={props.teams} value={team} onChange={value => setTeam(value)} />
                 <Button>Criar Card</Button>
             </form>
         </section>
